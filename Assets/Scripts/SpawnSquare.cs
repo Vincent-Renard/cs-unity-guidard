dd188b00881f90ac2f54f9808915e9d8a7830cec
Romain Guidez <romain.guidez@etu.univ-orleans.fr>
2020-01-02 19:29:36 +0100

    Add prefab for square

--- Assets/Scripts/SpawnSquare.cs
+++ Assets/Scripts/SpawnSquare.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class spawn : MonoBehaviour
+public class SpawnSquare : MonoBehaviour
 {
-    private BoxCollider2D bc;
+    public GameObject squarePrefab;
+
     // Start is called before the first frame update
     void Start()
     {
-        bc= GetComponent<BoxCollider2D>();
+        Instantiate(squarePrefab, new Vector3(0, 0, 0), Quaternion.identity);
     }
 
     // Update is called once per frame
