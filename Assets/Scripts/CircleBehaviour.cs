92a85f5dd22343d47eec6b213260d888a99a9221
Romain Guidez <romain.guidez@etu.univ-orleans.fr>
2020-01-03 17:19:53 +0100

    Add ropes

--- Assets/Scripts/CircleBehaviour.cs
+++ Assets/Scripts/CircleBehaviour.cs
@@ -7,16 +7,23 @@ public class CircleBehaviour : MonoBehaviour
     private Vector3 screenPoint;
     private Vector3 offset;
 
+    private bool attached;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        attached = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (attached) {
+            var connected1 = GetComponents<SpringJoint2D>()[0].connectedBody;
+            var connected2 = GetComponents<SpringJoint2D>()[1].connectedBody;
+            connected1.GetComponent<LineRenderer>().SetPositions(new Vector3[]{new Vector3(connected1.position.x, connected1.position.y, 0), new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, 0)});
+            connected2.GetComponent<LineRenderer>().SetPositions(new Vector3[]{new Vector3(connected2.position.x, connected2.position.y, 0), new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, 0)});
+        }
     }
 
     void OnMouseDown(){
@@ -31,6 +38,7 @@ public class CircleBehaviour : MonoBehaviour
 	}
 
     void OnMouseUp() {
+        attached = false;
         foreach (SpringJoint2D component in GetComponents<SpringJoint2D>()) {
             Destroy(component);
         }
