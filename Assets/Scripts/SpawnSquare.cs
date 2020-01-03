82b7f77d85b5f0b3e04a107fd40a3c2e5cb5de1d
viren <vincent.renard@gmx.com>
2020-01-03 16:25:54 +0100

    Now we have 6 dropping

--- Assets/Scripts/SpawnSquare.cs
+++ Assets/Scripts/SpawnSquare.cs
@@ -6,12 +6,14 @@ public class SpawnSquare : MonoBehaviour
     public float Nsec;
     public Vector3 SpawnLocation0;
     public Vector3 SpawnLocation1;
+    public Vector3 SpawnLocation2;
     public Vector3 SpawnLocation3;
     public Vector3 SpawnLocation4;
+    public Vector3 SpawnLocation5;
     private Vector3[] locations;
     // Start is called before the first frame update
     void Start(){
-        locations = new Vector3[] { SpawnLocation0, SpawnLocation1, SpawnLocation3, SpawnLocation4 };
+        locations = new Vector3[] { SpawnLocation0, SpawnLocation1, SpawnLocation2, SpawnLocation3, SpawnLocation4, SpawnLocation5 };
         int i = 0;
         foreach (Vector3 l in locations)
         {
@@ -46,6 +48,12 @@ public class SpawnSquare : MonoBehaviour
             case 3:
                 InvokeRepeating("spw3", i, Nsec);
                 break;
+            case 4:
+                InvokeRepeating("spw4", i, Nsec);
+                break;
+            case 5:
+                InvokeRepeating("spw5", i, Nsec);
+                break;
         }  }
     private void spw0()
 
@@ -58,11 +66,21 @@ public class SpawnSquare : MonoBehaviour
 {
         Instantiate(squarePrefab, locations[1], Quaternion.identity);
              }
-    private void spw2()
+     private void spw2()
     {
         Instantiate(squarePrefab, locations[2], Quaternion.identity);
       }
-    private void spw3(){
+            private void spw3(){
         Instantiate(squarePrefab, locations[3], Quaternion.identity);
-         }
+        }
+                private void spw4()
+            {
+        Instantiate(squarePrefab, locations[4], Quaternion.identity);
+                                    }
+
+
+    private void spw5(){Instantiate(squarePrefab, locations[5], Quaternion.identity);
+    }
+
+   
 }
