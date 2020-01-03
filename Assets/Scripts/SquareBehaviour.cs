cfdd6cbe7269609857ed3685aa2865d43fc65f7d
Vincent <vincent.renard@gmx.com>
2020-01-03 14:56:52 +0100

    Add SquareBehaviour.cs

---
+++ Assets/Scripts/SquareBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquareBehaviour : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+    private BoxCollider2D bc2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        bc2D = GetComponent<BoxCollider2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+  }
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        Debug.Log(collider.name+" collision");
+    }
+    private void OnDestroy()
+    {
+        
+    }
+}
