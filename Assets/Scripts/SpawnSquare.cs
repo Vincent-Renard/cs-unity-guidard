0c914dcf405e991ddc02bac558afcef5279afafd
viren <vincent.renard@gmx.com>
2020-01-05 13:58:07 +0100

    Random droppping of carreys

--- Assets/Scripts/SpawnSquare.cs
+++ Assets/Scripts/SpawnSquare.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
 
+using System.Collections.Generic;
+
 public class SpawnSquare : MonoBehaviour
 {
     public GameObject squarePrefab;
     private Vector3 InitialPos;
-    public float Nsec;
+    public int NTicks;
     public float SpawnLocation0;
     public float SpawnLocation1;
     public float SpawnLocation2;
@@ -12,22 +14,25 @@ public class SpawnSquare : MonoBehaviour
     public float SpawnLocation4;
     public float SpawnLocation5;
     private float[] locations;
+    private List<int> activeSpawners;
 
     // Start is called before the first frame update
     void Start()
     {
         InitialPos.y = GetComponent<Transform>().position.y;
         InitialPos.z = GetComponent<Transform>().position.z;
-        this.Resync();
+        activeSpawners = new List<int>();
+        Resync();
     }
 
     public void Resync()
     {
+        activeSpawners.Clear();
         locations = new float[] { SpawnLocation0, SpawnLocation1, SpawnLocation2, SpawnLocation3, SpawnLocation4, SpawnLocation5 };
         int i = 0;
         foreach (float l in locations) {
             if (!Equals(l, 0.0f)) {
-                spawn(i);
+                activeSpawners.Add(i);
             }
             i++;
         }
@@ -36,53 +41,19 @@ public class SpawnSquare : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-
-    }
-
-    private void spawn(int i)   {
-        switch (i) {
-            case 0:
-                InvokeRepeating("spw0", i, Nsec);
-                break;
-            case 1:
-                InvokeRepeating("spw1", i, Nsec);
-                break;
-            case 2:
-                InvokeRepeating("spw2", i, Nsec);
-                break;
-            case 3:
-                InvokeRepeating("spw3", i, Nsec);
-                break;
-            case 4:
-                InvokeRepeating("spw4", i, Nsec);
-                break;
-            case 5:
-                InvokeRepeating("spw5", i, Nsec);
-                break;
+        var size = activeSpawners.Count;
+       int i =  Random.Range(0, size*NTicks);
+        if (i < size)
+        {
+            Spawn(activeSpawners[i]);
         }
+       
     }
+   
 
-    private void spw0() {
-        spw(0);
-    }
-    private void spw1() {
-        spw(1);
-    }
-    private void spw2() {
-        spw(2);
-    }
-    private void spw3() {
-        spw(3);
-    }
-    private void spw4() {
-        spw(4);
-    }
-    private void spw5() {
-        spw(5);
-    }
-    private void spw(int i) {
-        GameObject square = Instantiate(squarePrefab, new Vector3(locations[i], InitialPos.y, InitialPos.z), Quaternion.identity);
+    private void Spawn(int iSpawner) {
+        
+        GameObject square = Instantiate(squarePrefab, new Vector3(locations[iSpawner], InitialPos.y, InitialPos.z), Quaternion.identity);
         square.tag = "enemy";
     }
 }
